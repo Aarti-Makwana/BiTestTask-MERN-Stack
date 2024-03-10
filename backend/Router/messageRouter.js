b1e0c32e002c3f447d7255ed92ae6aa4c4fd8397
aarti <aartimakwana2408@gmail.com>
2024-03-10 17:19:56 +0530

    delete folders

--- backend/Router/messageRouter.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { addMessageController,getMessagesController } from "../controller/msgController.js";
-const messageRouter =express.Router();
-messageRouter.post("/",addMessageController);
-messageRouter.get("/:chatId",getMessagesController);
-export default messageRouter;
