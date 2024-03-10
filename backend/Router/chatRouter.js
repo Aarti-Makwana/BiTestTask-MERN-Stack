8bfb87ac9d0a7b73b2c7179dd7cffd4153f5359f
aarti <aartimakwana2408@gmail.com>
2024-03-10 17:41:11 +0530

    delete folders

--- backend/Router/chatRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { createChatController,userChatsController,findChatController } from "../controller/chatController.js";
-const chatRouter =express.Router();
-chatRouter.post("/",createChatController)
-chatRouter.get("/:userId",userChatsController)
-chatRouter.get("/find/:firstId/:secondId",findChatController)
-
-export default chatRouter;
--- backend/Router/userRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { userRegistrationController,userLoginController,showUserController } from '../controller/userController.js';
-const userRouter = express.Router();
-userRouter.post("/userRegistration",userRegistrationController);
-userRouter.post("/userLogin",userLoginController);
-userRouter.get("/showUser",showUserController);
-export default userRouter;
